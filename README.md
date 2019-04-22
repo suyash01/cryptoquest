ec53264b6675d006cd9fb488ec92401008d96b78
Suyash Mittal <suyash8514@gmail.com>
2019-04-22 13:38:18 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # CryptoQuest
 
-Install the dependencies
+1. Install the dependencies
 
-Run `npm start` to start the server
+2. Create an `.env` file in the root folder with the following properties
+
+   - PORT - Port for the server
+   - SECRET - passphrase for the jwt
+   - DBURL - URL for the `mongodb` database
+
+3. Run `npm start` to start the server
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // Connecting to the DB
-mongoose.connect('mongodb://localhost/cryptoquest', { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.DBURL, { useNewUrlParser: true, useCreateIndex: true });
 
-mongoose.connection.on('open', () => {
-    console.log("Connected to the DB");
+mongoose.connection.on("open", () => {
+  console.log("Connected to the DB");
 });
 
-mongoose.connection.on('error', () => {
-    console.log("Error connecting to the DB");
+mongoose.connection.on("error", () => {
+  console.log("Error connecting to the DB");
 });
 
 const user = require("./routes/user");
 const chals = require("./routes/chals");
 
 // Middleware Setup
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Importing Routes
 app.use("/user", user);
@@ -32,20 +32,20 @@ app.use("/chals", chals);
 
 // Setting Up Error Messages and Status
 app.use((req, res, next) => {
-    const error = new Error("Not Found");
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        status: 0,
-        error: error.message
-    });
+  res.status(error.status || 500);
+  res.json({
+    status: 0,
+    error: error.message
+  });
 });
 
 // Starting the Server
 app.listen(port, () => {
-    console.log("Listening on port: " + port);
+  console.log("Listening on port: " + port);
 });
